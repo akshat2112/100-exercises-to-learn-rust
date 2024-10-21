ac31d897c3b8467eb2ece46dc1426591ea640dc9
akshat2112 <akshat2112@gmail.com>
2024-10-21 14:04:55 -0700

    Exercises day 6

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,17 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()){
+        Ok(ticket) => ticket,
+        Err(error_message) => {
+            if error_message.contains("Description") {
+                Ticket::new(title.clone(), "Description not provided".to_string(), status.clone()).unwrap()
+            }
+            else {
+                panic!("{error_message}");
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
